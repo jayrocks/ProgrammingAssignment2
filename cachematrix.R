77cf5ac80dbe0d99a553d317323c5dc82507f83e
jayrocks <jayrocks@users.noreply.github.com>
2014-10-30 23:44:34 +0530

    Update cachematrix.R
    This is just the modification to the cached matrix function.....

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The set and get functions are for setting and obtaining the values of the cached inverse matrix of x...
+## The setMatrix and getMatrix functions are for setting and obtaining the value of the inverse matrix of x
 
-## Write a short comment describing this function
+## Generating the cached inverse matrix of x.....and then obtaining the value of the same...
+## Setting and obtaining the value of the inverse matrix of x.....
 
 makeCacheMatrix <- function(x = matrix()) {
+
+invMat <- NULL  # invMat....is the Inverse Matrix
+  
+  set <- function(y) {
+    x <<- y
+    invMat <<- NULL
+    return(x)
+  }
+  
+  get <- function(){
+     x
+  }
+  
+  setMatrix <- function(inverseMat){
+    m <<- inverseMat
+  }  
+    
+  getMatrix <- function(){
+    m
+  }  
+    
+  list(set = set, get = get,
+       setmean = setMatrix,
+       getmean = getMatrix)
 
 }
 
 
-## Write a short comment describing this function
+## Check if the cached value of the inverse of the matris of x is available then, getting and returning tat value...or else the creation of the new cached value of the inverse matrix of x.....
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+         m <- x$getMatrix()
+  
+  if(!is.null(m)) {
+    message("getting cached Inverse Matrix")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setMatrix(m)
+  m
 }
